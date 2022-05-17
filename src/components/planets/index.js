d24cc25ea5c9fbeb53e751121d97bee28cdd3165
Gustavo Dias <dev.gustavo@hotmail.com>
2022-05-17 08:31:14 -0300

    metodo antigo react sem hook

---
+++ src/components/planets/index.js
@@ -0,0 +1,71 @@
+import React, {Fragment} from 'react';
+import Planet from './planet';
+
+/*const showMessage = () => {
+    console.log("Meu primeiro evento");
+}
+*/
+
+async function getPlanets() {
+    let response = await fetch('http://localhost:3000/api/planets.json');
+    let data = await response.json();
+    return data;
+}
+
+
+class Planets extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            planets: []
+        }
+    }
+
+    componentDidMount() {
+        getPlanets().then(data => {
+            this.setState(state => ({
+                planets: data['planets']
+            }))
+        })
+    }
+
+    removeLast = () => {
+        let new_planets = [...this.state.planets];
+        new_planets.pop();
+        this.setState(state => ({
+            planets: new_planets
+        }))
+    }
+
+    duplicateLastPlanet = () => {
+        let last_planets = this.state.planets[this.state.planets.length -1];
+        this.setState(state => ({
+            planets: [...this.state.planets, last_planets]
+        }))
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <h3>Planet List</h3>
+                <button onClick={this.removeLast}>Remove Last</button>
+                <button onClick={this.duplicateLastPlanet}>Duplicate Last</button>
+                <hr/>
+                
+                {this.state.planets.map((planet, index) =>
+                    <Planet id={planet.id}
+                            name={planet.name}
+                            description={planet.description}
+                            img_url={planet.img_url}
+                            link={planet.link}
+                            key={index}
+                    />
+                )}
+                
+            </Fragment>
+        )
+    }
+}
+
+
+export default Planets;
---
+++ src/components/planets/planet/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import GrayImg from '../../shared/gray_img';
+import DescriptionWithLink from '../../shared/description_with_link';
+
+async function getSatellites(id) {
+    let response = await fetch(`http://localhost:3000/api/${id}.json`);
+    let data = await response.json();
+    return data;
+}
+
+class Planet extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            satellites: []
+        }
+    }
+
+    componentDidMount() {
+        getSatellites(this.props.id).then(data => {
+            this.setState(state => ({
+                satellites: data['satellites']
+            }))
+        })
+    }
+
+    render() {
+
+        let title;
+        if(this.props.title_with_underline)
+            title = <h4><u>{this.props.name}</u></h4>
+        else
+            title = <h4>{this.props.name}</h4>
+
+        return (
+            <div>
+                {title}
+                <DescriptionWithLink description={this.props.description} link={this.props.link}/>
+                <GrayImg img_url={this.props.img_url} gray={this.props.gray}></GrayImg>
+    
+                <h4>Satélites</h4>
+                <ul>
+                    {this.state.satellites.map((satellites, index) =>
+                    <li key={index}>{satellites.name}</li>)}
+                </ul>
+                <hr/>
+            </div>
+        )
+    }
+}
+
+/*
+const Planet = (props) => {
+    const names = ['a', 'b', 'c', 'd', 'e', 'f'];
+    const satellites = names.map((n) =>
+        <li>Satélite {n}</li>
+    )
+
+
+
+}
+*/
+
+export default Planet;
